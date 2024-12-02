6016378d0e78af1c145c18c56889ca1821cd060a
trollzero <80132943+trollzero@users.noreply.github.com>
2024-12-02 01:31:37 -0500

    implemented the get balances function, the transaction function for both regular transactions and the bonus. implemented the crash simulations initially very naively but now I have also implemented the following funcitonality: after each prepare phase when everyone has prepare we create an entry for the prepare log and append it there. when everyone has prepared we send a log prepare rpc that makes all the participants save the prepare in persistent memory including the coordinator. this way in case of a crash the coordinator can see if the transaction was logged or not and proceed with recovery or whatever. now techinically everything except the Raft replication part should be done properly. to see any of the simulations check the usage in the client_2pc file.

--- client_2pc.py
+++ client_2pc.py
@@ -8,7 +8,7 @@ class Client2PC(BaseClient):
     Extended client class for Two-Phase Commit (2PC) functionality.
     """
 
-    def perform_transaction(self, transactions):
+    def perform_transaction(self, transactions, bonus = False, simulation_num = 0):
         """
         Send a transaction request to the coordinator.
         The transaction format is a dictionary where keys are node names
@@ -24,11 +24,21 @@ class Client2PC(BaseClient):
         # WE SHOULD BE ABLE TO CHECK THE STATUS OF THE TRANSACTION IN BETWEEN THE PREPARATION AND COMMIT
         # WE SHOULD BE ABLE TO CHECK THE STATUS OF THE TRANSACTION AFTER THE COMMIT
         
+        if bonus == True:
+            print("Performing transaction with bonus...")
+            balance_a, balance_b, bones_value = self.calculate_bonus()
+            print(f"Balance A: {balance_a}, Balance B: {balance_b}, Bonus Value: {bones_value}")
+        
         coordinator = 'node1'  # Assuming node1 is the coordinator
         coordinator_info = NODES[coordinator]
 
-        print(f"Sending transaction to coordinator: {transactions}")
-        response = self.send_rpc(coordinator_info['ip'], coordinator_info['port'], '2pc_request', transactions)
+        if bonus == True and bones_value != None:
+            transactions = {'node2': bones_value, 'node3': bones_value}
+            print(f"Sending bonus transaaction to coordinator: {transactions}")
+            response = self.send_rpc(coordinator_info['ip'], coordinator_info['port'], '2pc_request', {'transactions': transactions, 'simulation_num': simulation_num})
+        else:
+            print(f"Sending transaction to coordinator: {transactions}")
+            response = self.send_rpc(coordinator_info['ip'], coordinator_info['port'], '2pc_request', {'transactions': transactions, 'simulation_num': simulation_num})
 
         if response and response.get('status') == 'committed':
             print("Transaction successfully committed.")
@@ -36,6 +46,29 @@ class Client2PC(BaseClient):
             print("Transaction aborted.")
         else:
             print("Failed to process the transaction.")
+            
+    def calculate_bonus(self):
+        bonus_value = None
+        balance_a = None
+        balance_b = None
+        
+        for node_name in NODES:
+            node_info = NODES[node_name]
+            response = self.send_rpc(node_info['ip'], node_info['port'], 'GetBalance', {})
+            if response and response.get('status') == 'success':
+                print(f"Successfully calculated bonus for {node_name}")
+            else:
+                print(f"Failed to calculate bonus for {node_name}")
+            if response and node_name == 'node2':
+                balance_a = response.get('balance')
+                if balance_a > 0:
+                    bonus_value = balance_a * 0.2
+                else:
+                    print('Balance is not greater than 0')
+            elif response and node_name == 'node3':
+                balance_b = response.get('balance')
+        
+        return balance_a, balance_b, bonus_value  
 
     def check_transaction_status(self):
         """
@@ -63,11 +96,21 @@ class Client2PC(BaseClient):
             node_info = NODES[node_name]
             response = self.send_rpc(node_info['ip'], node_info['port'], 'GetBalance', {})
             if response:
-                # WE ARE RECEIVING A NUMBER AS THE BALANCE SO CHANGE
-                balance = response.get('balance', 'Unknown')
+                balance = response.get('balance', 'Could not be retrieved.')
                 print(f"Account balance at {node_name}: {balance}")
             else:
                 print(f"Failed to fetch balance from {node_name}")
+    
+    def set_account_balance(self, node, balance):
+        """
+        Set the account balance for a participant node.
+        """
+        node_info = NODES[node]
+        response = self.send_rpc(node_info['ip'], node_info['port'], 'SetBalance', {'balance': balance})
+        if response and response.get('status') == 'success':
+            print(f"Successfully set balance for {node} to {balance}")
+        else:
+            print(f"Failed to set balance for {node}")
 
 if __name__ == '__main__':
     client = Client2PC()
@@ -77,9 +120,10 @@ if __name__ == '__main__':
         print("  python client_2pc.py leader_change")
         print("  python client_2pc.py simulate_crash [node_name]")
         print("  python client_2pc.py print_logs")
-        print("  python client_2pc.py transaction '{\"node2\": -100, \"node3\": 100}'")
+        print("  python client_2pc.py transaction trans_val1 trans_val2 [bonus flag (optional)] [node crash simulation # (Default = 0)]")
         print("  python client_2pc.py check_status")
         print("  python client_2pc.py get_balances")
+        print("  python client_2pc.py set_balance [node_name] [balance]")
         sys.exit(1)
 
     command = sys.argv[1]
@@ -104,12 +148,16 @@ if __name__ == '__main__':
 
     # New commands for 2PC functionality
     elif command == 'transaction':
-        if len(sys.argv) != 3:
-            print("Usage: python client_2pc.py transaction '{\"node2\": -100, \"node3\": 100}'")
+        if len(sys.argv) < 4 or len(sys.argv) > 6:
+            print("Usage: python client_2pc.py transaction trans_val1 trans_val2 [bonus flag (optional)]")
             sys.exit(1)
         try:
-            transactions = json.loads(sys.argv[2])
-            client.perform_transaction(transactions)
+            node2_val = int(sys.argv[2])
+            node3_val = int(sys.argv[3])
+            transactions = {'node2': node2_val, 'node3': node3_val}
+            bonus_flag = True if len(sys.argv) > 4 and sys.argv[4] == 'bonus' else False
+            crash_simulation_num = sys.argv[5] if len(sys.argv) == 6 else 0
+            client.perform_transaction(transactions, bonus_flag, simulation_num=crash_simulation_num)
         except json.JSONDecodeError:
             print("Invalid transaction format. Use JSON format.")
             sys.exit(1)
@@ -117,5 +165,12 @@ if __name__ == '__main__':
         client.check_transaction_status()
     elif command == 'get_balances':
         client.get_account_balances()
+    elif command == 'set_balance':
+        if len(sys.argv) != 4:
+            print("Usage: python client_2pc.py set_balance [node_name] [balance]")
+            sys.exit(1)
+        node_name = sys.argv[2]
+        balance = int(sys.argv[3])
+        client.set_account_balance(node_name, balance)
     else:
         print("Unknown command.")
--- node_2pc.py
+++ node_2pc.py
@@ -1,20 +1,49 @@
 import socket
 import json
 from node import Node
-
+from config import NODES
 class TwoPhaseCommitNode(Node):
     def __init__(self, name, role):
         super().__init__(name)
         self.role = role  # Coordinator or Participant
         self.account_balance = 0
+        self.transaction_id = 0
+        self.transaction_status = None
+        self.prepare_log = []
+        self.prepare_log_file = f'{self.name}_prepare_log.json'
         self.account_file = f"{self.name}_account.txt"
         if role == "Participant":
             self.load_account_balance()
+        self.load_prepare_log()
+        
+    def load_prepare_log(self):
+        try:
+            with open(self.prepare_log_file, 'r') as f:
+                self.prepare_log = json.load(f)
+                self.transactio_id = self.prepare_log[-1]['transaction_id'] + 1
+                if not isinstance(self.prepare_log, list):
+                    self.prepare_log = []
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.prepare_log = []
+            
+    def save_prepare_log(self):
+        try:
+            with open(self.prepare_log_file, 'r') as f:
+                logs = json.load(f)
+                if not isinstance(logs, list):
+                    logs = []
+        except (FileNotFoundError, json.JSONDecodeError):
+            logs = []
+
+        logs.append(self.prepare_log[-1])
+
+        with open(self.prepare_log_file, 'w') as f:
+            json.dump(logs, f)
 
     def load_account_balance(self):
         try:
             with open(self.account_file, 'r') as f:
-                self.account_balance = int(f.read().strip())
+                self.account_balance = float(f.read().strip())
         except FileNotFoundError:
             with open(self.account_file, 'w') as f:
                 f.write("0")
@@ -25,38 +54,147 @@ class TwoPhaseCommitNode(Node):
             f.write(str(self.account_balance))
     
     def get_account_balance(self):
-        return self.account_balance
+        balance = self.account_balance
+        return{'node_name': self.name, 'balance': balance}
+    
+    def set_account_balance(self, value):
+        self.account_balance = value
+        self.save_account_balance()
+        print(f"Account balance set to: {value}")
+        return {'status': 'success'}
 
     def prepare_transaction(self, delta):
         if self.account_balance + delta < 0:
+            print('Insufficient funds. Aborting transaction.')
             return False
         return True
     
     def check_transaction_status(self):
-        # JUST SOMETHING TO HAVE IT DEFINED
-        return {'status': 'committed' if self.account_balance >= 0 else 'aborted'}
+        if self.role == 'Coordinator':
+            transaction_status = self.transaction_status
+        return {'transaction_status': transaction_status}
 
     def commit_transaction(self, delta):
         self.account_balance += delta
         self.save_account_balance()
+    
+    def prepare_log_entry(self, data):
+        log_entry = {}
+        log_entry['transaction_id'] = self.transaction_id
+        log_entry['simulation_num'] = data['simulation_num']
+        log_entry['transactions'] = data['transactions']
+        self.transaction_id += 1
+        return log_entry
 
     def handle_2pc_prepare(self, data):
-        delta = data.get('delta', 0)
+        delta = data['transactions'][self.name]
+        simulation_num = int(data.get('simulation_num', 0))
+        print(f'simulation_num during prepare: {simulation_num}')
+        
+        if simulation_num == 1:
+            print('Simulation 1: Participant crashes before preparing transaction')
+            return {'status': 'abort'}
+        
         if self.prepare_transaction(delta):
+            log_entry = self.prepare_log_entry(data)
+            self.prepare_log.append(log_entry)
             return {'status': 'prepared'}
         return {'status': 'abort'}
 
     def handle_2pc_commit(self, data):
-        delta = data.get('delta', 0)
+        delta = delta = data['transactions'][self.name]
+        simulation_num = int(data.get('simulation_num', 0))
+        print(f'simulation_num during commit: {simulation_num}')
+        
+        if simulation_num == 2:
+            print('Simulation 2: Participant crashes before committing transaction')
+            return {'status': 'abort'}
+        
         self.commit_transaction(delta)
         return {'status': 'committed'}
 
+    def handle_2pc_log_prepare(self, data):
+        self.save_prepare_log()
+        return {'status': 'logged_prepare'}
+    
     def handle_2pc_request(self, data):
-        if data['phase'] == 'prepare':
-            return self.handle_2pc_prepare(data)
-        elif data['phase'] == 'commit':
-            return self.handle_2pc_commit(data)
-        return {'status': 'error', 'message': 'Unknown phase'}
+        if self.role == 'Coordinator':
+            num_participants = len(data)
+            num_prepared = 0
+            num_logged_prepare = 0
+            num_committed = 0
+            num_logged_commit = 0
+            coordinator_node_name = 'node1'
+            self.transaction_status = 'started'
+            print(f"Starting 2PC transaction with {num_participants} participants.")
+            print(f'Simulation number: {data["simulation_num"]}')
+            
+            for node_name in NODES:
+                if node_name != coordinator_node_name:
+                    node_info = NODES[node_name]
+                    print(f"Sending prepare request to  participant: {node_name}")
+
+                    response = self.send_rpc(node_info['ip'], node_info['port'], '2pc_prepare', data)
+                    if response and response.get('status') == 'prepared':
+                        print(f"Participant {node_name} is prepared.")
+                        num_prepared += 1
+                        if num_prepared == num_participants:
+                            for node_name in NODES:
+                                if node_name != coordinator_node_name:
+                                    node_info = NODES[node_name]
+                                    print(f"Sending log prepare consencus to  participant: {node_name}")
+
+                                    response = self.send_rpc(node_info['ip'], node_info['port'], '2pc_log_prepare', data)
+                                    if response and response.get('status') == 'logged_prepare':
+                                        print(f"Participant {node_name} logged the prepare request.")
+                                        num_logged_prepare += 1
+                                        if num_logged_prepare == num_participants:
+                                            log_entry = self.prepare_log_entry(data)
+                                            self.prepare_log.append(log_entry)
+                                            self.save_prepare_log()
+                                    else:
+                                        print(f"Participant {node_name} did not log prepare request.")
+                                        return {'status': 'logging prepare aborted'}
+                    else:
+                        self.transaction_status = 'aborted'
+                        print(f"Participant {node_name} is not prepared.")
+                        return {'status': 'prepare aborted'}
+            
+
+            
+            if int(data['simulation_num']) == 3:
+                print('Simulation 3: Coordinator crashes before sending commit requests and did not log the transaction. Client is informed to start a new transaction.')
+                return {'status': 'aborted'}
+            
+            if int(data['simulation_num']) == 4:
+                print('Simulation 4: Coordinator crashes but can recover because he logged the transaction status and can continue after the preparation phase.')
+            
+                
+                    
+            if num_prepared == num_participants:
+                self.transaction_status = 'prepared'
+                print("All participants are prepared. Sending commit requests.")
+                
+                for node_name in NODES:
+                    if node_name != coordinator_node_name:
+                        node_info = NODES[node_name]
+                        print(f"Sending commit request to participant: {node_name}")
+                        
+                        response = self.send_rpc(node_info['ip'], node_info['port'], '2pc_commit', data)
+                        if response and response.get('status') == 'committed':
+                            print(f"Participant {node_name} has committed.")
+                            num_committed += 1
+                        else:
+                            self.transaction_status = 'commit aborted'
+                            print(f"Participant {node_name} has not committed.")
+                            return {'status': 'aborted'}
+            
+            if num_committed == num_participants:
+                self.transaction_status = 'committed'
+                print("All participants have committed.")
+                return {'status': 'committed'}
+            
+        return {'status': 'error', 'message': 'Transaction not committed'}
 
     def handle_client_connection(self, client_socket: socket.socket):
         """
@@ -91,9 +229,18 @@ class TwoPhaseCommitNode(Node):
                     elif rpc_type == 'PrintLog':
                         # Handle print log request
                         response = self.print_node_log()
-                    elif rpc_type in ['2pc_prepare', '2pc_commit']:
+                    elif rpc_type == '2pc_request':
                         # Delegate to 2PC-specific handler
-                        response = self.handle_2pc_request(request['data'])    
+                        response = self.handle_2pc_request(request['data'])  
+                    elif rpc_type == '2pc_prepare':
+                        # Delegate to 2PC-specific handler
+                        response = self.handle_2pc_prepare(request['data'])
+                    elif rpc_type == '2pc_commit':
+                        # Delegate to 2PC-specific handler
+                        response = self.handle_2pc_commit(request['data'])  
+                    elif rpc_type == '2pc_log_prepare':
+                        # Delegate to 2PC-specific handler
+                        response = self.handle_2pc_log_prepare(request['data'])
                     elif rpc_type == 'GetBalance':
                         # Handle client balance requests
                         # MAYBE THIS IS NOT CORRECT
@@ -101,6 +248,9 @@ class TwoPhaseCommitNode(Node):
                     elif rpc_type == 'CheckTransactionStatus':
                         # Handle transaction status check requests
                         response = self.check_transaction_status()
+                    elif rpc_type == 'SetBalance':
+                        # Handle setting the account balance
+                        response = self.set_account_balance(request['data']['balance'])
                     else:
                         response = {'error': 'Unknown RPC type'}
 
